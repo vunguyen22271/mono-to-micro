a6b5467fc4690a557ab3e01c8c00faabc229ff96
Mac <mac@192.168.1.12>
2024-02-07 11:30:10 +0700

    feat: add Proxy CI

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6542bfe502154f7cbcd98c48ce30cbf-1892719817.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
